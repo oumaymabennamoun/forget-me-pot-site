4879fe42c86c443518f717bc0c3458dd77232b57
oumayma <oumayma.bennamoun@outlook.com>
2025-12-28 15:24:27 -0500

    new port.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const path = require("path");
 
 const app = express();
-const PORT = 4000;
+const PORT = 4001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
